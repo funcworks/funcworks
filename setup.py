462ce2d993fd7bf00f0011086634e02b04851d92
Adam Kimbler <akimbler@fiu.edu>
2020-03-06 22:41:11 -0500

    Update setup.py
    Updated Reqs in setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup, find_packages
 setup(
-    name = 'funcworks',
-    packages = find_packages(),
+    name='funcworks',
+    packages=find_packages(),
     entry_points={'console_scripts': [
-            'funcworks=funcworks.cli.run:main'
-        ]}
-)
+        'funcworks=funcworks.cli.run:main'
+        ]},
+    install_requires=["nipype>=1.4.2", "traits==5.2.0", "pybids>=0.10.1"])
