a6f1f543aab6b19102674e3294d98a2f4f78782c
adamkimbler <akimb009@fiu.edu>
2020-03-11 09:56:44 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
 from setuptools import setup, find_packages
+
 setup(
     name='funcworks',
+    version='0.0.1a'
     packages=find_packages(),
     entry_points={'console_scripts': [
         'funcworks=funcworks.cli.run:main'
