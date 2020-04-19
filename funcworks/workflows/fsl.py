8459908a4691d05f6ba2d3a673245867a5af5825
akimbler <akimb009@fiu.edu>
2020-04-19 08:33:07 -0400

    Update fsl.py

--- funcworks/workflows/fsl.py
+++ funcworks/workflows/fsl.py
@@ -73,6 +73,7 @@ def fsl_run_level_wf(model,
 
     wrangle_volumes = pe.MapNode(
         IdentityInterface(fields=['functional_file']),
+        iterfield=['functional_file'],
         name='wrangle_volumes'
     )
 
